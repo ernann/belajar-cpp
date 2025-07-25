5aaa306843a116c216e4b60aaed4701c822aedd2
ernann <ernanwana@gmail.com>
2025-07-25 19:13:35 +0700

    Create ern2.cpp

---
+++ ern2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int berat_badan = 70;  
+
+    if (berat_badan > 70) {
+        cout << "Kamu Gendut" << endl;
+    } else {
+        cout << "Berat Kamu Normal" << endl;
+    }
+cin.get();
+}
