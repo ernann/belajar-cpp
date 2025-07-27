dfb2583caf81cb9b57cdae43715ca6e20dfe06b3
ernann <ernanwana@gmail.com>
2025-07-27 22:06:28 +0700

    Create ern6.cpp

---
+++ ern6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int W;
+	W = 1;
+	cout << endl;
+	cout << "      MENAMPILKAN TULISAN 2 BARIS" << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << endl;
+	while(W <=5)
+    {
+		cout << "Yang Ke = " << W << endl;
+		cout << "Selamat Datang" << endl;
+		cout << "----MinGW----" << endl;
+		cout << endl;
+		W++;    
+	}
+cin.get();
+}
