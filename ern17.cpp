513a14e1a2ada9ecc3710d7c99ae23d29a30646a
ernann <ernanwana@gmail.com>
2025-08-03 23:20:18 +0700

    Create ern17.cpp

---
+++ ern17.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int JumtDt_ernan=3;
+	int Nilai [JumtDt_ernan] = {22, 7,1966};
+	int i, Total=0;
+	cout << endl;
+	cout << "Menginput ARRAY/LARIK 1 Dimensi " << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	for (i=0; i < JumtDt_ernan; i++)
+	{
+		cout << " Isi Elemen Ke-" << i+1 <<" : " << Nilai [i]<< endl;
+		Total = Total + Nilai [i];
+		
+	}
+	cout << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "Total dari Penjumlahan = " << Total << endl;
+	cin.get();
+}
+	#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int JumtDt_ernan=3;
+	int Nilai [JumtDt_ernan] = {22, 7,1966};
+	int i, Total=0;
+	cout << endl;
+	cout << "Menginput ARRAY/LARIK 1 Dimensi " << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	for (i=0; i < JumtDt_ernan; i++)
+	{
+		cout << " Isi Elemen Ke-" << i+1 <<" : " << Nilai [i]<< endl;
+		Total = Total + Nilai [i];
+		
+	}
+	cout << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "Total dari Penjumlahan = " << Total << endl;
+	cin.get();
+}
+	
